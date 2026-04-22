31ad1f02bc38cd94f47640c0278e4a19958928b5
Avishka Madushan <avishkamadushan4338@gmail.com>
2026-04-22 15:36:18 +0530

    Replace placeholder pages with ComingSoon component

--- frontend/src/pages/Departments/Departments.jsx
+++ frontend/src/pages/Departments/Departments.jsx
@@ -1,3 +1,2 @@
-export default function Departments() {
-  return <div className="page-container container"></div>
-}
+import ComingSoon from '../../components/ComingSoon'
+export default function Departments() { return <ComingSoon pageKey="departments" /> }
--- frontend/src/pages/Notices/Notices.jsx
+++ frontend/src/pages/Notices/Notices.jsx
@@ -1,3 +1,2 @@
-export default function Notices() {
-  return <div className="page-container container"></div>
-}
+import ComingSoon from '../../components/ComingSoon'
+export default function Notices() { return <ComingSoon pageKey="notices" /> }
