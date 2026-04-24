be25d9c19488134b3d6f8063a17eaca1daa1ae24
Avishka Madushan <avishkamadushan4338@gmail.com>
2026-04-24 20:53:02 +0530

    Update App.jsx routing and layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
+import { FooterProvider } from './contexts/FooterContext'
 import MainLayout from './layouts/MainLayout'
 import LandingPage from './pages/Landing/LandingPage'
 import Home from './pages/Home/Home'
-import About from './pages/About/About'
+import AboutSecretary from './pages/AboutSecretary/AboutSecretary'
 import Departments from './pages/Departments/Departments'
 import Documents from './pages/Documents/Documents'
 import News from './pages/News/News'
@@ -11,10 +12,12 @@ import Tenders from './pages/Tenders/Tenders'
 import Gallery from './pages/Gallery/Gallery'
 import FAQ from './pages/FAQ/FAQ'
 import Contact from './pages/Contact/Contact'
+import ComingSoon from './components/ComingSoon'
 import NotFound from './pages/NotFound'
 
 export default function App() {
   return (
+    <FooterProvider>
     <Routes>
       {/* Language-selection gateway – standalone, no Navbar/Footer */}
       <Route path="/" element={<LandingPage />} />
@@ -22,7 +25,16 @@ export default function App() {
       {/* Main site – wrapped in Navbar + Footer layout */}
       <Route element={<MainLayout />}>
         <Route path="home" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="about-secretary" element={<AboutSecretary />} />
+        <Route path="about/ministry-overview" element={<ComingSoon pageKey="aboutMinistryOverview" />} />
+        <Route path="about/minister" element={<ComingSoon pageKey="aboutMinister" />} />
+        <Route path="about/deputy-minister-education" element={<ComingSoon pageKey="aboutDeputyMinisterEducation" />} />
+        <Route path="about/deputy-minister-vocational" element={<ComingSoon pageKey="aboutDeputyMinisterVocational" />} />
+        <Route path="about/secretary" element={<AboutSecretary />} />
+        <Route path="about/organization-structure" element={<ComingSoon pageKey="aboutOrganizationStructure" />} />
+        <Route path="about/functions-duties" element={<ComingSoon pageKey="aboutFunctionsDuties" />} />
+        <Route path="about/history" element={<ComingSoon pageKey="aboutHistory" />} />
+        <Route path="about/affiliated-institutions" element={<ComingSoon pageKey="aboutAffiliatedInstitutions" />} />
         <Route path="departments" element={<Departments />} />
         <Route path="documents" element={<Documents />} />
         <Route path="news" element={<News />} />
@@ -34,5 +46,6 @@ export default function App() {
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
+    </FooterProvider>
   )
 }
