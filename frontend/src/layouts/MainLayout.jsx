9c339ad69c5b15569957706e6361ff0cf22662ec
Avishka Madushan <avishkamadushan4338@gmail.com>
2026-04-24 20:51:57 +0530

    Update MainLayout with new styles

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,16 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { useFooter } from '../contexts/FooterContext'
 import './MainLayout.css'
 
 export default function MainLayout() {
+  const { hidden } = useFooter()
   return (
     <div className="layout">
       <Navbar />
       <main className="main-content pt-[76px]">
         <Outlet />
       </main>
-      <Footer />
+      {!hidden && <Footer />}
     </div>
   )
 }
